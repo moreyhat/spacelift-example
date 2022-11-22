fc79acdceac03801cbc3ecb4edbeb93aebd26cd8
moreyhat <moreyhat@gmail.com>
2022-11-21 16:54:21 -0800

    Make bucket private

--- Pulumi/public-s3-bucket/index.ts
+++ Pulumi/public-s3-bucket/index.ts
@@ -3,9 +3,7 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket", {
-    acl: "public-read"
-});
+const bucket = new aws.s3.Bucket("my-bucket", {});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
